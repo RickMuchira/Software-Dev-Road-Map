81089200c3657c887242bde5bd139c67c28edd78
RickMuchira <rickcharlesm@gmail.com>
2025-11-04 21:52:30 +0300

    Add variable.c with examples of int, float, and char types and update myfirstprogram.c

---
+++ c/variable.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Integer variable
+    int myInt = 42;
+    printf("Integer value: %d\n", myInt);
+    
+    // Float variable
+    float myFloat = 3.14;
+    printf("Float value: %f\n", myFloat);
+    
+    // Character variable
+    char myChar = 'A';
+    printf("Character value: %c\n", myChar);
+    
+    return 0;
+}
+
