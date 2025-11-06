09828950dad54a88385a82e57134b9a4ca460749
RickMuchira <rickcharlesm@gmail.com>
2025-11-06 09:35:27 +0300

    area eample,ovewriting ,variable

---
+++ c/area.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    int lenght=5, width=2, area;
+    area=lenght*width;
+    printf("area of this rectangle %d" ,area);
+    return 0;
+    }
--- c/multivar.c
+++ c/multivar.c
@@ -5,3 +5,11 @@ int main(){
     printf("the sum of this 3 numbers is %d",x+y+z);
     return 0;
 }
+/*
+The general rules for naming variables are:
+
+Names can contain letters, digits and underscores
+Names must begin with a letter or an underscore (_)
+Names are case-sensitive (myVar and myvar are different variables)
+Names cannot contain whitespaces or special characters like !, #, %, etc.
+Reserved words (such as int) cannot be used as names*/
