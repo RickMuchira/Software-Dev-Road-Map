b964ecc8ed01f923ac75c8ddd73c6d3333d2bbbd
RickMuchira <rickcharlesm@gmail.com>
2025-12-26 22:36:59 +0300

    THr bennininging

--- C++/myfirst.cpp
+++ C++/myfirst.cpp
@@ -1,7 +1,11 @@
+//is a header file library that lets us work with input and output objects, such as cout
 #include <iostream>
+// means that we can use names for objects and variables from the standard library.
 using namespace std;
 
 int main() {
+  //cout (pronounced "see-out") is an object used together with the insertion operator (<<) to output/print text. In our example, it will output "Hello World!".
   cout << "Hello World!";
+  cout << "Merry Christmas";
   return 0;
 }
