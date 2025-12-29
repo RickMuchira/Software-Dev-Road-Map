6a9e87ca25a5488d3d60646bf9a4e702d04cfdbf
RickMuchira <rickcharlesm@gmail.com>
2025-12-29 12:58:50 +0300

    string

--- C++/string.cpp
+++ C++/string.cpp
@@ -9,6 +9,14 @@ int main(){
     string song = "Never Gonna Give You Up";
 
     cout<< "This Artist : " << rick << " sang the song " <<song <<endl;
+//This Artist : Rick Astley sang the song Never Gonna Give You Up
+
+//concatenate strings +
+string firstName = "John ";
+string lastName = "Doe";
+string fullName = firstName + lastName;
+cout << "Full Name: " << fullName << endl;
+//Full Name: John Doe
     return 0; 
 }
 
