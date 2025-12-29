639ec9927b71476b412cd6483de32a8710c4c13b
RickMuchira <rickcharlesm@gmail.com>
2025-12-29 12:46:11 +0300

    song

---
+++ C++/string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    string rick = "Rick Astley";
+    string song = "Never Gonna Give You Up";
+
+    cout<< "This Artist : " << rick << " sang the song " <<song <<endl;
+    return 0; 
+}
+
+//
