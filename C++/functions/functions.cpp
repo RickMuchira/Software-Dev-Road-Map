b0cde9a1326371f3945a272981dc20dc96726d26
RickMuchira <rickcharlesm@gmail.com>
2026-01-09 13:33:25 +0300

    How to write a function

---
+++ C++/functions/functions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//function decalration
+void myfuncton();
+
+int main(){
+    myfuncton();//function calling 
+    return 0;
+}
+//function definition
+void myfuncton(){
+cout<<"free your mind";
+}
